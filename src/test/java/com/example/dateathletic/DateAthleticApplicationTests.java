4ce646a02b0da067315c35c5ca183d628a83dc51
Patrik <patrikjo95@gmail.com>
2023-01-11 14:26:29 +0100

    Initial commit

---
+++ src/test/java/com/example/dateathletic/DateAthleticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dateathletic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateAthleticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
