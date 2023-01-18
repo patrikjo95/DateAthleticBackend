f932e20c6c096e9ae2e77f3861ea2ec9a2e862ea
KillianBoy <Killian.walsh123@hotmail.com>
2023-01-19 00:02:24 +0100

    Killian commit

--- backend/src/main/java/com/dateathletic/backend/utils/jwt/Jwt.java
+++ backend/src/main/java/com/dateathletic/backend/utils/jwt/Jwt.java
@@ -3,6 +3,7 @@ package com.dateathletic.backend.utils.jwt;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 @Component
+@RequiredArgsConstructor
 public class Jwt {
     @Value("${secret.jwt-key}")
     protected String KEY;
