b6b55c83781470cf12954aa5ad55742bf5e51245
KillianBoy <Killian.walsh123@hotmail.com>
2023-01-18 16:03:34 +0100

    Killian commit

--- backend/src/main/java/com/dateathletic/backend/dto/UpdateUserDto.java
+++ backend/src/main/java/com/dateathletic/backend/dto/UpdateUserDto.java
@@ -1,11 +1,14 @@
 package com.dateathletic.backend.dto;
 
+import lombok.NonNull;
+
 public record UpdateUserDto(
 
+        @NonNull
         String username,
-
+        @NonNull
         String email,
-
+        @NonNull
         String password
 
 
