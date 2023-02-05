0d76c11a53bdcb6db1b60b8e2da26794f83b917a
KillianBoy <Killian.walsh123@hotmail.com>
2023-02-05 21:40:24 +0100

    Killian commit

--- backend/src/main/java/com/dateathletic/backend/controller/auth/UpdateController.java
+++ backend/src/main/java/com/dateathletic/backend/controller/auth/UpdateController.java
@@ -1,40 +1,31 @@
 package com.dateathletic.backend.controller.auth;
 
 
+
 import com.dateathletic.backend.dto.UpdateUserDto;
 import com.dateathletic.backend.dto.UpdateUserInfoDto;
-import com.dateathletic.backend.service.UserServiceInfo;
 import com.dateathletic.backend.service.userservice.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import static com.dateathletic.backend.BackendApplication.USER_API;
+
 
 @RestController
-@RequestMapping( path = USER_API + "/update", produces = "application/json;")
+@RequestMapping(path ="/update", produces = "application/json;")
 @RequiredArgsConstructor
 public class UpdateController {
 
-    private final UserServiceInfo userServiceInfo;
+
 
     private final UserService userService;
 
 
-
-    @PutMapping(value = "/User{id}")
-    public ResponseEntity<String> updateUser (@PathVariable Long id, @RequestBody UpdateUserDto dto) {
-
-        userService.updateUser(id, dto);
-
-        return new ResponseEntity<>("Nu har du uppdaterat din profil", HttpStatus.OK);
-    }
-
     @PutMapping(value = "/UserInfo{id}")
-    public ResponseEntity<String> updateUserInfo (@PathVariable Long id, @RequestBody UpdateUserInfoDto dto) {
+    public ResponseEntity<String> updateUserInfo (@PathVariable Long id, @RequestBody UpdateUserDto dto) {
 
-        userServiceInfo.updateUserInfo(id, dto);
+        userService.updateUserInfoById(id, dto);
 
         return new ResponseEntity<>("Nu har du uppdaterat dina personuppgifter", HttpStatus.OK);
     }
