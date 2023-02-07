09f5b96221a15d3638376c30f2c2d0c1a1d18de7
Erkan <erkan.ec.cicek@gmail.com>
2023-02-07 16:50:22 +0100

    added stuff to controllers/ bryan

--- backend/src/main/java/com/dateathletic/backend/dto/UserDisplayDto.java
+++ backend/src/main/java/com/dateathletic/backend/dto/UserDisplayDto.java
@@ -2,15 +2,18 @@ package com.dateathletic.backend.dto;
 
 import com.dateathletic.backend.domain.User;
 
-public record UserDisplayDto(String username, String firstname, String lastname, String dob, String bio, String preferences, Long uid) {
+import java.util.List;
+
+public record UserDisplayDto(String username, String firstname, String lastname, String dob, String interests, String bio, List<String> imagePath, Long uid) {
     public static UserDisplayDto mapToUserDisplayDto(User user) {
         return new UserDisplayDto(
                 user.getUsername(),
                 user.getUserInfo().getFirstname(),
                 user.getUserInfo().getLastname(),
                 user.getUserInfo().getDoB(),
-                user.getUserInfo().getBio(),
                 user.getUserInfo().getInterests(),
+                user.getUserInfo().getBio(),
+                user.getProfileImg().getImagePaths(),
                 user.getId());
     }
 }
